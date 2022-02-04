a8c3bba0216bd93aca6861357b4d12df11583c45
Ka Hou, CHOI <kz1412@link.cuhk.edu.hk>
2022-02-04 21:04:09 +0800

    fix: scanf check

--- atm_proj/atms.c
+++ atm_proj/atms.c
@@ -4,27 +4,21 @@
 #include <stdio.h>
 
 FILE *masterFile;
-int validatingUser;
-
-char userAcctInfo[17];
-char balanceSign[2];
-char strBalance[16];
-double intBalance;
-
-int chosenService;
-
-char targetAcctInfo[17];
-
+char userAcctNumber[17], targetAcctNumber[17], chosenService;
+long double userBalance = -1;
 double transacAmount;
 
-int masterFileExists()
+int VALIDATING_USER = 1;
+int VALIDATING_TARGET = 0;
+
+int checkMasterFileExists()
 {
     if ((masterFile = fopen("master.txt", "r")))
     {
         fclose(masterFile);
-        return 1;
+        return EXIT_SUCCESS;
     }
-    return 0;
+    return EXIT_FAILURE;
 }
 
 void atmInitialize()
@@ -38,21 +32,44 @@ void atmInitialize()
 
 int chooseAtm()
 {
-    int userAtmChoice = -1;
-    printf("=> PLEASE CHOOSE THE ATM\n"
-           "=> PRESS 1 FOR ATM 711\n"
-           "=> PRESS 2 FOR ATM 713\n");
-    scanf("%d", &userAtmChoice);
-    while (getchar() != '\n')
-        ;
-    return userAtmChoice;
+    char input[100];
+
+    while (1)
+    {
+        printf("=> PLEASE CHOOSE THE ATM\n"
+               "=> PRESS 1 FOR ATM 711\n"
+               "=> PRESS 2 FOR ATM 713\n");
+        scanf("%s", &input);
+        while (getchar() != '\n')
+            ;
+        printf("%s\n", input);
+
+        if (strcmp(input, "1") && strcmp(input, "2"))
+        {
+            printf("=> INVALID INPUT\n");
+            memset(input, '\0', 100);
+            continue;
+        }
+        break;
+    }
+
+    if (strcmp(input, "1") == 0)
+    {
+        return 711;
+    }
+    else if (strcmp(input, "2") == 0)
+    {
+        return 713;
+    }
+    else
+    {
+        return '\0';
+    }
 }
 
-int validateAcctInfo(char *inputAcctInfo)
+int validateAcctInfo(int validatingUser, char *inAcctInfo)
 {
-    char nameBuffer[100];
-    char acctInfoBuffer[100];
-    char tmp[2];
+    char nameBuffer[50], acctInfoBuffer[50], userBalanceSign[2], strUserBalance[16];
 
     masterFile = fopen("master.txt", "r");
     while (fgets(nameBuffer, 21, masterFile))
@@ -60,8 +77,8 @@ int validateAcctInfo(char *inputAcctInfo)
         if (validatingUser)
         {
             fgets(acctInfoBuffer, 23, masterFile);
-            fgets(balanceSign, 2, masterFile);
-            fgets(strBalance, 16, masterFile);
+            fgets(userBalanceSign, 2, masterFile);
+            fgets(strUserBalance, 16, masterFile);
         }
         else
         {
@@ -70,24 +87,26 @@ int validateAcctInfo(char *inputAcctInfo)
         while (getc(masterFile) != '\n')
             ;
 
-        if (strcmp(acctInfoBuffer, inputAcctInfo) == 0)
+        if (strcmp(acctInfoBuffer, inAcctInfo) == 0)
         {
             printf("\n");
             printf("==>  HOLDER: %s \n", nameBuffer);
             printf("==>    ACCT: %.16s \n", acctInfoBuffer);
             if (validatingUser)
             {
-                printf("==> BALANCE: %s%s \n", balanceSign, strBalance);
+                userBalance = atoll(strUserBalance) / 100.0;
+                userBalance = (strcmp(userBalanceSign, "+") == 0) ? userBalance : 0 - userBalance;
+                printf("==> BALANCE: %+016.02Lf \n", userBalance);
             }
             printf("\n");
 
             fclose(masterFile);
-            if ((!validatingUser) && strcmp(acctInfoBuffer, userAcctInfo) == 0)
+            if ((!validatingUser) && strcmp(acctInfoBuffer, userAcctNumber) == 0)
             {
                 printf("=> YOU CANNOT TRANSFER TO YOURSELF\n");
-                return 0;
+                return EXIT_FAILURE;
             }
-            return 1;
+            return EXIT_SUCCESS;
         }
     }
 
@@ -101,92 +120,99 @@ int validateAcctInfo(char *inputAcctInfo)
     }
 
     fclose(masterFile);
-    return 0;
+    return EXIT_FAILURE;
 }
 
-int inputAcctInfo()
+void inputAcctInfo(int validatingUser)
 {
-    char inputAcctInfo[100];
-    char inputUserAcctNumber[100];
-    char userAcctPassword[100];
+    char inAcctInfo[100], inUserAcctNumber[100], inUserAcctPassword[100];
 
     do
     {
         if (validatingUser)
         {
-
             printf("=> ACCOUNT\n");
-            scanf("%s", inputUserAcctNumber);
+            scanf("%s", inUserAcctNumber);
             while (getchar() != '\n')
                 ;
+            printf("%s\n", inUserAcctNumber);
 
             printf("=> PASSWORD\n");
-            scanf("%s", userAcctPassword);
+            scanf("%s", inUserAcctPassword);
             while (getchar() != '\n')
                 ;
+            printf("%s\n", inUserAcctPassword);
 
-            strcpy(inputAcctInfo, inputUserAcctNumber);
-            strcat(inputAcctInfo, userAcctPassword);
+            strcpy(inAcctInfo, inUserAcctNumber);
+            strcat(inAcctInfo, inUserAcctPassword);
         }
         else
         {
-            printf("=> TARGET ACCOUT\n");
-            scanf("%s", inputAcctInfo);
+            printf("=> TARGET ACCOUNT\n");
+            scanf("%s", inAcctInfo);
             while (getchar() != '\n')
                 ;
+            printf("%s\n", inAcctInfo);
         }
-    } while (validateAcctInfo(inputAcctInfo) == 0);
+    } while (validateAcctInfo(validatingUser, inAcctInfo) == EXIT_FAILURE);
 
     if (validatingUser)
     {
-        strcpy(userAcctInfo, inputUserAcctNumber);
+        strcpy(userAcctNumber, inUserAcctNumber);
     }
     else
     {
-        strcpy(targetAcctInfo, inputAcctInfo);
+        strcpy(targetAcctNumber, inAcctInfo);
     }
-    memset(inputAcctInfo, '\0', 100);
 
-    return 0;
+    memset(inAcctInfo, '\0', 100);
 }
 
-char chooseService()
+void chooseService()
 {
     char input[100];
-    printf("=> PLEASE CHOOSE YOUR SERVICE\n"
-           "=> PRESS D FOR DEPOSIT\n"
-           "=> PRESS W FOR WITHDRAWAL\n"
-           "=> PRESS T FOR TRANSFER\n");
-    scanf("%s", input);
-    while (getchar() != '\n')
-        ;
-    if (strlen(input) != 1)
+
+    while (1)
     {
-        printf("=> strlen != 1 but == %d\n", strlen(input));
-        return '\0';
+        printf("=> PLEASE CHOOSE YOUR SERVICE\n"
+               "=> PRESS D FOR DEPOSIT\n"
+               "=> PRESS W FOR WITHDRAWAL\n"
+               "=> PRESS T FOR TRANSFER\n");
+        scanf("%s", input);
+        while (getchar() != '\n')
+            ;
+        printf("%s\n", input);
+
+        if (strcmp(input, "D") && strcmp(input, "W") && strcmp(input, "T"))
+        {
+            printf("=> INVALID INPUT\n");
+            memset(input, '\0', 100);
+            continue;
+        }
+        break;
     }
-    return input[0];
+
+    chosenService = input[0];
 }
 
 void inputAmount()
 {
     transacAmount = -1;
-    intBalance = atoi(strBalance) / 100.0;
     while (1)
     {
         printf("=> AMOUNT\n");
         scanf("%lf", &transacAmount);
         while (getchar() != '\n')
             ;
+        printf("%lf\n", transacAmount);
 
-        printf("==> OPERATION: %lf %c %lf\n\n", intBalance, chosenService, transacAmount);
-
+        printf("\n==> OPERATION: %+016.02Lf %c %08.02lf\n\n", userBalance, chosenService, transacAmount);
         if (transacAmount < 0)
         {
             printf("=> INVALID INPUT\n");
             continue;
         }
-        else if (chosenService != 'D' && transacAmount > intBalance)
+        else if (chosenService != 'D' && transacAmount > userBalance)
         {
             printf("=> INSUFFICIENT BALANCE\n");
             continue;
@@ -195,115 +221,91 @@ void inputAmount()
     }
 }
 
-int outputRecord(){
-
-}
-
 char chooseIfContinue()
 {
     char input[100];
-    printf("=> CONTINUE?\n"
-           "=> N FOR NO\n"
-           "=> Y FOR YES\n");
-    scanf("%s", input);
-    while (getchar() != '\n')
-        ;
-    if (strlen(input) != 1)
+
+    while (1)
     {
-        printf("=> strlen != 1 but == %d\n", strlen(input));
-        return '\0';
+        printf("=> CONTINUE?\n"
+               "=> N FOR NO\n"
+               "=> Y FOR YES\n");
+        scanf("%s", input);
+        while (getchar() != '\n')
+            ;
+        printf("%s\n", input);
+
+        if (strcmp(input, "Y") && strcmp(input, "N"))
+        {
+            printf("=> INVALID INPUT\n");
+            memset(input, '\0', 100);
+            continue;
+        }
+        break;
     }
+
     return input[0];
 }
 
 int main()
 {
-    FILE *t71_One = fopen("trans711.txt", "w");
-    FILE *t71_Three = fopen("trans713.txt", "w");
+    FILE *t71_One_File = fopen("trans711.txt", "w");
+    FILE *t71_Three_File = fopen("trans713.txt", "w");
 
     int timeStamp = 0;
-    char chosenIf = 'Y';
+    char chosenIfContinue = 'Y';
 
-    while (chosenIf == 'Y')
+    do
     {
-        if (!masterFileExists())
+        if (checkMasterFileExists() == 1)
         {
-            printf("[ABORT]: MASTER FILE NOT FOUND");
-            return 1;
+            printf("[ABORT]: MASTER FILE NOT FOUND\n\n");
+            return EXIT_FAILURE;
         }
 
         atmInitialize();
 
         int chosenAtm = chooseAtm();
-        while (chosenAtm != 1 && chosenAtm != 2)
-        {
-            printf("=> INVALID INPUT\n");
-            chosenAtm = chooseAtm();
-        }
 
-        validatingUser = 1;
-        inputAcctInfo();
-        if (validatingUser && strcmp(balanceSign, "-") == 0)
+        inputAcctInfo(VALIDATING_USER);
+        if (userBalance < 0)
         {
             printf("=> NEGATIVE REMAINS TRANSACTION ABORT\n");
             continue;
         }
 
-        chosenService = chooseService();
-        while (chosenService != 'D' && chosenService != 'W' && chosenService != 'T')
-        {
-            printf("=> INVALID INPUT\n");
-            chosenService = chooseService();
-        }
-
+        chooseService();
         if (chosenService == 'T')
         {
-            validatingUser = 0;
-            inputAcctInfo();
+            inputAcctInfo(VALIDATING_TARGET);
         }
+
         inputAmount();
 
         printf("==> WRITTEN RECORD: \n");
-        char writeStr[30];
-        char chosenAtmName[8];
-        FILE * chosenAtmFile;
-        if (chosenAtm == 1)
-        {
-            chosenAtmFile = t71_One;
-            strcpy(chosenAtmName, "ATM-711");
-        }
-        else
-        {
-            chosenAtmFile = t71_Three;
-            strcpy(chosenAtmName, "ATM-713");
-        }
-        if (chosenService == 'T')
-        {   
-            sprintf(writeStr, "%s%c%07d%05d\n", userAcctInfo, 'W', (int)(transacAmount * 100), timeStamp++);
-            fputs( writeStr, chosenAtmFile);
-            sprintf(writeStr, "%s%c%07d%05d\n", targetAcctInfo, 'D', (int)(transacAmount * 100), timeStamp++);
-            fputs( writeStr, chosenAtmFile);
 
-            printf("==> [%s]: %s %c %07d %05d\n", chosenAtmName, userAcctInfo, 'W', (int)(transacAmount * 100), timeStamp);
-            printf("==> [%s]: %s %c %07d %05d\n", chosenAtmName, targetAcctInfo, 'D', (int)(transacAmount * 100), timeStamp);
+        FILE *chosenAtmFile = (chosenAtm == 711) ? t71_One_File : t71_Three_File;
+
+        if (chosenService == 'T')
+        {
+            chosenService = 'W';
+            fprintf(chosenAtmFile, "%s%c%07.0lf%05d\n", userAcctNumber, chosenService, transacAmount * 100.0, timeStamp++);
+            printf("==> [ATM-%d]: %s %c %07.0lf %05d\n", chosenAtm, userAcctNumber, chosenService, transacAmount * 100.0, timeStamp);
+            
+            chosenService = 'D';
+            fprintf(chosenAtmFile, "%s%c%07.0lf%05d\n", targetAcctNumber, chosenService, transacAmount * 100.0, timeStamp++);
+            printf("==> [ATM-%d]: %s %c %07.0lf %05d\n", chosenAtm, targetAcctNumber, chosenService, transacAmount * 100.0, timeStamp);
         }
         else
         {
-            sprintf(writeStr, "%s%c%07d%05d\n", userAcctInfo, chosenService, (int)(transacAmount * 100), timeStamp++);
-            fputs( writeStr, chosenAtmFile);
-            printf("==> [%s]: %s %c %07d %05d\n", chosenAtmName, userAcctInfo, chosenService, (int)(transacAmount * 100), timeStamp);
+            fprintf(chosenAtmFile, "%s%c%07.0lf%05d\n", userAcctNumber, chosenService, transacAmount * 100.0, timeStamp++);
+            printf("==> [ATM-%d]: %s %c %07.0lf %05d\n", chosenAtm, userAcctNumber, chosenService, transacAmount * 100.0, timeStamp);
         }
         printf("\n");
 
-        chosenIf = chooseIfContinue();
-        while (chosenIf != 'Y' && chosenIf != 'N')
-        {
-            printf("=> INVALID INPUT\n");
-            chosenIf = chooseIfContinue();
-        }
-    }
+    } while (userBalance < 0 || chooseIfContinue() == 'Y');
 
-    fclose(t71_One);
-    fclose(t71_Three);
-    return 0;
+    fclose(t71_One_File);
+    fclose(t71_Three_File);
+    return EXIT_SUCCESS;
 }
